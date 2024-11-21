bdc29d8da4007b7a67abc1f6e8a7d12fd1e5fffb
Mohit Raj <mohit@agrmohit.com>
2024-11-21 23:01:19 +0530

    feat: complete mini tutorial on homepage

---
+++ README.md
@@ -0,0 +1,13 @@
+# FastAPI api-one
+
+Code to learn [FastAPI](https://fastapi.tiangolo.com)
+
+## API Documentation
+
+FastAPI has Swagger and Redoc support built-in. I like [Scalar](https://github.com/scalar/scalar) so I am using it
+
+Assuming the default dev server url [localhost:8000](http://localhost:8000), the endpoints are:
+
+- [Scalar `/scalar`](http://localhost:8000/scalar)
+- [Swagger `/docs`](http://localhost:8000/docs)
+- [Redoc `/redoc`](http://localhost:8000/redoc)
--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 from typing import Union
 
 from fastapi import FastAPI
+from pydantic import BaseModel
+from scalar_fastapi import get_scalar_api_reference
 
 app = FastAPI()
+
+
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: Union[bool, None] = None
 
 
 @app.get("/")
@@ -10,6 +18,19 @@ def read_root():
     return {"Hello": "World"}
 
 
+@app.get("/scalar", include_in_schema=False)
+async def scalar_html():
+    return get_scalar_api_reference(
+        openapi_url=app.openapi_url,
+        title=app.title,
+    )
+
+
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+
+@app.put("/items/{item_id}")
+def update_item(item_id: int, item: Item):
+    return {"item_name": item.name, "item_id": item_id}
